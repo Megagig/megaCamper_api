626d5539a68e19eaf42dd575ad0c7b514ec8efaf
Megagig <megagigsoftwaresolutions.com>
2024-04-27 18:40:55 +0100

    Creating Routes Responses in express

--- server.js
+++ server.js
@@ -8,6 +8,32 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, message: 'Show all bootcamps' });
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, message: `Get  bootcamp ${req.params.id}` });
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, message: 'create new  bootcamps' });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, message: `Update  bootcamp ${req.params.id}` });
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, message: `Delete  bootcamps ${req.params.id}` });
+});
+
 // Define port
 const PORT = process.env.PORT || 5000;
 
