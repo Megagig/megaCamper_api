41b0ebde5eb13d50ef754bd3aba2caf9f9e172a6
Megagig <megagigsoftwaresolutions.com>
2024-04-30 13:45:24 +0100

    Add colors in the console using colors.js

--- config/db.js
+++ config/db.js
@@ -4,9 +4,11 @@ const mongoose = require('mongoose');
 connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {});
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(
+      `MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold
+    );
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(`Error: ${error.message}`.red);
     process.exit(1);
   }
 };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
 const connectDB = require('./config/db');
 
 //require the bootcamps file, which contains all the routes
@@ -29,5 +30,7 @@ const PORT = process.env.PORT || 5000;
 // Start server
 
 app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  );
 });
